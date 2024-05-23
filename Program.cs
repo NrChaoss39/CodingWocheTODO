2063810d969e3fb4eeb718e5c22aa0354b7ef5c0
Mario Koessler <mkoessler@bib-gmbh.de>
2024-05-23 09:32:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace CodingWoche;
+
+using System.Xml.Schema;
+using ToDoMethods;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(
+            " #####     ####     ###    ######   #######           ########  #####   #####     #####   \r\n" +
+            "##   ##   ##  ##   ## ##    ##  ##   ##   #           ## ## ## ### ###   ## ##   ### ###  \r\n" +
+            "##       ##       ##   ##   ##  ##   ##                  ##    ##   ##   ##  ##  ##   ##  \r\n" +
+            " #####   ##       ##   ##   #####    ####                ##    ##   ##   ##  ##  ##   ##  \r\n" +
+            "     ##  ##       #######   ## ##    ##                  ##    ##   ##   ##  ##  ##   ##  \r\n" +
+            "##   ##   ##  ##  ##   ##   ## ##    ##   #              ##    ### ###   ## ##   ### ###  \r\n" +
+            " #####     ####   ##   ##  #### ##  #######             ####    #####   #####     #####   \r\n");
+
+        string path = "C:\\Users\\M02419\\OneDrive - Nürnberger Baugruppe GmbH + Co KG\\Dokumente\\CodingWoche\\TODOList.txt";
+        int i = 0;
+        string toDoOptions = "Hello!\n" +
+                "What do you want to do?\n" +
+                "[S]ee all TODOs\n" +
+                "[C]hange a TODO\n" +
+                "[A]dd a TODO\n" +
+                "[R]emove a TODO\n" +
+                "[E]xit";
+        while (i == 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine(toDoOptions);
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            char upperInput = char.ToUpper(input);
+            switch (upperInput)
+            {
+                case 'S':
+                    ToDoMethods.ShowTODOs(path);
+                    break;
+                case 'C':
+                    ToDoMethods.ChangeTODO(path);
+                    break;
+                case 'A':
+                    ToDoMethods.AddTODO(path);
+                    break;
+                case 'R':
+                    ToDoMethods.RemoveTODO(path);
+                    break;
+                case 'E':
+                    Console.WriteLine();
+                    Console.WriteLine("Goodbye!");
+                    i = 1;
+                    break;
+                case 'T':
+                    ToDoMethods.TODO();
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+        }
+    }
+}
